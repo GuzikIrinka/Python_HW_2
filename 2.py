ec5959a8b23346cb0dca63d39aef87e745cfdefa
Irina <andrey_k.90@mail.ru>
2022-09-10 22:56:29 +0300

    решение задач №3,5

--- 2.py
+++ 2.py
@@ -22,11 +22,27 @@
 
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-from math import factorial
+# from math import factorial
+
+# N = int(input('Введите число: '))
+# factorial = 1
+# for i in range(1, N+1):
+#     factorial *= i
+# print(factorial)
 
 
-N = int(input('Введите число: '))
-factorial = 1
-for i in range(1, N+1):
-    factorial *= i
-print(factorial)
+#Задайте список из n чисел последовательности (1+1/n)**n
+# и выведите на экран их сумму.
+
+# n = int(input('Введите число: '))
+# sum = 0
+# for i in range (1, n+1):
+#     sum += (1 + 1/i)**i
+# print(sum) 
+
+
+#Реализуйте алгоритм перемешивания списка.
+import random
+l=[9,8,7,6,5,4,3,2,1]
+random.shuffle(l)
+print(l)
