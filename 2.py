60d7037b0fd07ea1f27166fca67ac1bacb61e99a
Irina <andrey_k.90@mail.ru>
2022-09-10 21:54:02 +0300

    решение задачи №2

--- 2.py
+++ 2.py
@@ -6,10 +6,27 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-num = input()
-sum = 0
+# num = input()
+# sum = 0
 
-for i in str(num):
-    if i != ".":
-        sum += int(i)
-print(sum)
+# for i in str(num):
+#     if i != ".":
+#         sum += int(i)
+# print(sum)
+
+
+# Напишите программу, которая принимает на вход число N и выдает
+# набор произведений чисел от 1 до N.
+
+# Пример:
+
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+from math import factorial
+
+
+N = int(input('Введите число: '))
+factorial = 1
+for i in range(1, N+1):
+    factorial *= i
+print(factorial)
